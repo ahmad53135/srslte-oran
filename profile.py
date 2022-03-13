71743e524852c03f26d5f1a24856f68c89e632cd
ahmad53135 <ahmadsepahi53@gmail.com>
2022-03-13 16:18:43 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -8,126 +8,120 @@ import geni.urn as URN
 
 tourDescription = """
 
-## srsLTE Controlled RF with shared vlan support (primarily for ORAN)
+# srsLTE Controlled RF
 
----
+Use this profile to intantiate an end-to-end LTE network in a controlled RF
+environment (wired connections between UE and eNB). The UE can be srsLTE-based
+or a Nexus 5.
 
-IMPORTANT: DO NOT start this expirment until you have first instantiated a
-companion O-RAN experiment via the following profile:
+If you elect to use a Nexus 5, these nodes will be deployed:
 
-  https://www.powderwireless.net/p/PowderProfiles/O-RAN
+* Nexus 5 (`rue1`)
+* Generic Compute Node w/ ADB image (`adbnode`)
+* Intel NUC5300/B210 w/ srsLTE eNB/EPC (`enb1`)
 
-Furthermore, DO NOT start the srsLTE services in that experiment as
-directed.  See the instructions in this profile for more information.
+If instead you choose to use an srsLTE UE, these will be deployed:
 
----
-
-Use this profile to instantiate an end-to-end srsLTE network in a controlled RF
-environment (wired connections between UE and eNB).
-
-The following resources will be allocated:
-
-  * Intel NUC5300/B210 w/ srsLTE UE(s) 
-    * 1 or 2, depending on "Number of UEs" parameter: `rue1`, `rue2`
-  * Intel NUC5300/B210 w/ srsLTE eNB/EPC (`enb1`)
+* Intel NUC5300/B210 w/ srsLTE (`rue1`) or
+* Intel NUC5300/B210 w/ srsLTE eNB/EPC (`enb1`)
 
 """
 
 tourInstructions = """
 
-### Prerequisites: ORAN Setup
-
-You should have already started up an O-RAN experiment connected to
-the same shared VLAN you specified during the "parameterize" step.
-Make sure it is up and fully deployed first - see the instructions
-included in that profile.  However, DO NOT start the srsLTE components
-or `kpimon` xApp as directed in those instructions.
-
-First, log in to `node-0` in your O-RAN experiment and edit
-`/local/profile-public/scp-kpimon-config-file.json` to change the
-eNodeB identifier.  Find the line that shows:
-
-```
-"ranList":"enB_macro_661_8112_0019b0"
-```
-
-And change it to:
-
-```
-"ranList":"enB_macro_14156_321_0019b0"
-```
-
-Don't forget to save the changes.
-
-Next, make note of the `e2term-sctp` service's IP address in the ORAN
-experiment.  To do that, open an SSH session to `node-0` in that
-experiment and run:
-
-```
-# Extract `e2term-sctp` IP address
-kubectl get svc -n ricplt --field-selector metadata.name=service-ricplt-e2term-sctp-alpha -o jsonpath='{.items[0].spec.clusterIP}'
-```
-
-You will need this address when starting the srsLTE eNodeB service.
-
 ### Start EPC and eNB
 
-Login to `enb1` via `ssh` and start the srsLTE EPC services:
+After your experiment becomes ready, login to `enb1` via `ssh` and do:
+
+```
+/local/repository/bin/start.sh
+```
+
+This will start a `tmux` session with three panes, running `srsepc` and
+`srsenb`, and then leaving your cursor in the last pane. After you've associated
+a UE with this eNB, you can use the third pane to run tests with `ping` or
+`iperf`. If you are not familiar with `tmux`, it's a terminal multiplexer that
+has some similarities to screen. Here's a [tmux cheat
+sheet](https://tmuxcheatsheet.com), but `ctrl-b o` (move to other pane) and
+`ctrl-b x` (kill pane), should get you pretty far. `ctrl-b d` will detach you
+from the `tmux` session and leave it running in the background. You can reattach
+with `tmux attach`.
+
+If you'd like to start `srsepc` and `srsenb` manually, here are the commands:
 
 ```
 # start srsepc
-sudo srsepc
+sudo srsepc /etc/srslte/epc.conf
+
+# start srsenb
+sudo srsenb /etc/srslte/enb.conf
 ```
 
-Then in another SSH session on `enb1`, start the eNB service.
-Substitute the IP address (or set it as an environment variable) in
-this command for the one captured for the `e2term-sctp` ORAN service
-in the previous step.
+### Nexus 5
+
+If you've deployed a Nexus 5, you should see it sync with the eNB eventually and
+obtain an IP address. Login to `adbnode` in order to access `rue1` via `adb`:
 
 ```
-# start srsenb (with agent connectivity to ORAN RIC)
-sudo srsenb --ric.agent.remote_ipv4_addr=${E2TERM_IP} --log.all_level=warn --ric.agent.log_level=debug --log.filename=stdout
+# on `adbnode`
+pnadb -a
+adb shell
 ```
 
-There will be output in srsenb and the O-RAN e2term mgmt and other
-service logs showing that the enb has connected to the O-RAN RIC.
-
-### Start the `kpimon` xApp
-
-Go back to the instructions in the O-RAN profile and follow the steps
-for starting the `kpimon` xApp (start with step 3 under "Running the
-O-RAN/srsLTE scp-kpimon demo").
-
-You should start seeing `srsenb` send periodic reports once the
-`kpimon` xApp starts.  These will appear in the `kpimon` xApp log
-output as well in the srsenb output.
-
-### Start the srsLTE UE
-
-SSH to `rue1` and run:
+Once you have an `adb` shell to `rue1`, you can use `ping` to test the
+connection, e.g.,
 
 ```
-sudo srsue
+# in adb shell connected to rue1
+# ping SGi IP
+ping 172.16.0.1
 ```
 
-You should see changes in the `kpimon` output when `srsue` is
-connected.  Try pinging `172.16.0.1` (the srs SPGW gateway address)
-from the UE and watch the `kpimon` counters tick.
+If the Nexus 5 fails to sync with the eNB, try rebooting it via the `adb` shell.
+After reboot, you'll have to repeat the `pnadb -a` and `adb shell` commands on
+`adbnode` to reestablish a connection to the Nexus 5.
+
+### srsLTE UE
+
+If you've deployed an srsLTE UE, login to `rue1` and do:
+
+```
+/local/repository/bin/start.sh
+```
+
+This will start a `tmux` session with two panes: one running srsue and the other
+holding a spare terminal for running tests with `ping` and `iperf`. Again, if
+you'd like to run `srsue` manually, do:
+
+```
+sudo srsue /etc/srslte/ue.conf
+```
 
 """
 
 
 class GLOBALS(object):
     NUC_HWTYPE = "nuc5300"
+    COTS_UE_HWTYPE = "nexus5"
     UBUNTU_1804_IMG = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD"
     SRSLTE_IMG = "urn:publicid:IDN+emulab.net+image+PowderProfiles:U18LL-SRSLTE:1"
 
+    COTS_UE_IMG = URN.Image(PN.PNDEFS.PNET_AM, "PhantomNet:ANDROID444-STD")
+    ADB_IMG = URN.Image(PN.PNDEFS.PNET_AM, "PhantomNet:UBUNTU14-64-PNTOOLS")
+
+
 pc = portal.Context()
-pc.defineParameter("num_ues", "Number of NUC+B210 srsLTE UEs to allocate",
-                   portal.ParameterType.INTEGER, 1, [1,2])
+pc.defineParameter("ue_type", "UE Type", portal.ParameterType.STRING, "nexus5",
+                   [("srsue", "srsLTE UE (B210)"), ("nexus5", "COTS UE (Nexus 5)")],
+                   longDescription="Type of UE to deploy.")
+
 pc.defineParameter("enb_node", "eNodeB Node ID",
                    portal.ParameterType.STRING, "", advanced=True,
                    longDescription="Specific eNodeB node to bind to.")
+
+pc.defineParameter("ue_node", "UE Node ID",
+                   portal.ParameterType.STRING, "", advanced=True,
+                   longDescription="Specific UE node to bind to.")
 
 pc.defineParameter(
     "multiplexLans", "Multiplex Networks",
@@ -173,10 +167,46 @@ enb1.component_id = params.enb_node
 enb1.hardware_type = GLOBALS.NUC_HWTYPE
 enb1.disk_image = GLOBALS.SRSLTE_IMG
 enb1.Desire("rf-controlled", 1)
+enb1_rue1_rf = enb1.addInterface("rue1_rf")
+# enb1.addService(rspec.Execute(shell="bash", command="/local/repository/bin/update-config-files.sh"))
+# enb1.addService(rspec.Execute(shell="bash", command="/local/repository/bin/tune-cpu.sh"))
+# enb1.addService(rspec.Execute(shell="bash", command="/local/repository/bin/add-nat-and-ip-forwarding.sh"))
+#
+# enb1.addService(rspec.Execute(shell="bash", command="/local/repository/bin/setup-ip-config.sh %s" % params.oranAddress))
+# enb1.addService(rspec.Execute(shell="bash", command="/local/repository/bin/setup-srslte.sh"))
+
 enb1.addService(rspec.Execute(shell="bash", command="/local/repository/bin/tune-cpu.sh"))
 enb1.addService(rspec.Execute(shell="bash", command="/local/repository/bin/setup-ip-config.sh %s" % params.oranAddress))
 enb1.addService(rspec.Execute(shell="bash", command="/local/repository/bin/update-config-files.sh"))
 enb1.addService(rspec.Execute(shell="bash", command="/local/repository/bin/setup-srslte.sh"))
+
+# Add a UE node
+"""if params.ue_type == "nexus5":
+    adbnode = request.RawPC("adbnode")
+    adbnode.disk_image = GLOBALS.ADB_IMG
+    rue1 = request.UE("rue1")
+    rue1.hardware_type = GLOBALS.COTS_UE_HWTYPE
+    rue1.disk_image = GLOBALS.COTS_UE_IMG
+    rue1.adb_target = "adbnode"
+elif params.ue_type == "srsue":
+    rue1 = request.RawPC("rue1")
+    rue1.hardware_type = GLOBALS.NUC_HWTYPE
+    rue1.disk_image = GLOBALS.SRSLTE_IMG
+    rue1.addService(rspec.Execute(shell="bash", command="/local/repository/bin/update-config-files.sh"))
+    rue1.addService(rspec.Execute(shell="bash", command="/local/repository/tune-cpu.sh"))
+"""
+
+adbnode = request.RawPC("adbnode")
+adbnode.disk_image = GLOBALS.ADB_IMG
+rue1 = request.UE("rue1")
+rue1.hardware_type = GLOBALS.COTS_UE_HWTYPE
+rue1.disk_image = GLOBALS.COTS_UE_IMG
+rue1.adb_target = "adbnode"
+
+rue1.component_id = params.ue_node
+rue1.Desire("rf-controlled", 1)
+rue1_enb1_rf = rue1.addInterface("enb1_rf")
+
 
 # Connect enb1 to shared vlan, if requested.
 if params.connectSharedVlan:
@@ -191,20 +221,10 @@ if params.connectSharedVlan:
         sharedvlan.link_multiplexing = True
         sharedvlan.best_effort = True
 
-# Add a srsLTE SDR-based UE nodes
-for i in range(1,params.num_ues+1):
-    ue = request.RawPC("rue%d" % i)
-    ue.hardware_type = GLOBALS.NUC_HWTYPE
-    ue.disk_image = GLOBALS.SRSLTE_IMG
-    ue.addService(rspec.Execute(shell="bash", command="/local/repository/bin/update-config-files.sh"))
-    ue.addService(rspec.Execute(shell="bash", command="/local/repository/tune-cpu.sh"))
-    ue.Desire("rf-controlled", 1)
-    # Create the RF link between the UE and eNodeB
-    rflink = request.RFLink("rflink%d" % i)
-    ue_enb1_rf = ue.addInterface("enb1_rf")
-    enb1_ue_rf = enb1.addInterface("rue%d_rf" % i)
-    rflink.addInterface(enb1_ue_rf)
-    rflink.addInterface(ue_enb1_rf)
+# Create the RF link between the UE and eNodeB
+rflink = request.RFLink("rflink")
+rflink.addInterface(enb1_rue1_rf)
+rflink.addInterface(rue1_enb1_rf)
 
 tour = IG.Tour()
 tour.Description(IG.Tour.MARKDOWN, tourDescription)
